0b950c82409da83a3d348da3806fe4bc7fda03b0
Ildefonso <ponchovalo@outlook.com>
2021-03-29 19:23:15 -0600

    cardsensidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  display: boolean = true;
+  sidebar: boolean = true;
 
   constructor() { }
-
+  
   ngOnInit(): void {
   }
 
